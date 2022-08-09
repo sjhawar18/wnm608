0c320ca7bebe0e50a061ca9a872c20cd8aaa8963
Shubhangi Jhawar <sjhawar2@art.edu>
2022-08-10 03:49:32 +0530

    module 15

--- jhawar.shubhangi/data/products.sql
+++ jhawar.shubhangi/data/products.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Jul 22, 2022 at 10:31 PM
+-- Generation Time: Aug 09, 2022 at 02:11 PM
 -- Server version: 5.7.38-cll-lve
 -- PHP Version: 7.3.32
 
@@ -46,18 +46,18 @@ CREATE TABLE `products` (
 --
 
 INSERT INTO `products` (`id`, `name`, `price`, `description`, `quantity`, `thumbnail`, `images`, `category`, `date_created`, `date_modified`) VALUES
-(1, 'Red Roses', 14.30, 'Pretty Red Roses', 25, 'red_rose.jpg', 'red_rose.jpg', 'flower', '2022-07-19 23:35:00', '2022-07-19 23:35:00'),
-(2, 'Lilies', 34.78, 'Lovely lilies', 45, 'lily.jpg', 'lily.jpg,lily_1.jpg', 'flower', '2022-07-19 23:38:58', '2022-07-19 23:38:58'),
-(3, 'Sunflower', 27.58, 'Sunshine sunflower', 41, 'sunflower.jpg', 'sunflower.jpg,sunflower_1.jpg', 'flower', '2022-07-19 23:42:24', '2022-07-19 23:42:24'),
-(4, 'The Flower of Love', 58.00, 'Bunch of roses and lilies', 19, 'flower_of_love.jpg', 'flower_of_love.jpg', 'bouquet', '2022-07-19 23:45:20', '2022-07-19 23:45:20'),
-(5, 'Peony', 19.67, 'Lovely Pink Peonies', 51, 'peony.jpg', 'peony.jpg,peony_1.jpg', 'flower', '2022-07-19 23:47:36', '2022-07-19 23:47:36'),
-(6, 'Tulip', 28.75, 'Basket of tulips', 38, 'tulip.jpg', 'tulip.jpg', 'bouquet', '2022-07-19 23:49:21', '2022-07-19 23:49:21'),
-(7, 'Aster', 13.93, 'Bunch of Aster', 84, 'aster.jpg', 'aster.jpg,aster_1.jpg', 'bouquet', '2022-07-19 23:51:13', '2022-07-19 23:51:13'),
-(8, 'Orchids', 67.14, 'Bunch of orchids', 46, 'orchid.jpg', 'orchid.jpg', 'bouquet', '2022-07-19 23:52:25', '2022-07-19 23:52:25'),
-(9, 'Daisy', 41.72, 'Basket of daisy', 67, 'daisy.jpg', 'daisy.jpg', 'bouquet', '2022-07-19 23:54:15', '2022-07-19 23:54:15'),
-(10, 'Mogra', 36.91, 'Beautiful basket of Mogra', 25, 'mogra.jpg', 'mogra.jpg', 'bouquet', '2022-07-19 23:56:38', '2022-07-19 23:56:38'),
-(11, 'Lotus', 31.43, 'Lovely lotus', 57, 'lotus.jpg', 'lotus.jpg', 'flower', '2022-07-19 23:59:10', '2022-07-19 23:59:10'),
-(12, 'Daffodil', 54.98, 'Fresh Daffodil', 39, 'daffodil.jpg', 'daffodil.jpg,daffodil_1.jpg', 'bouquet', '2022-07-20 00:01:13', '2022-07-20 00:01:13');
+(1, 'Red Roses', 14.30, 'Bunch of pretty red roses. Perfect for your loved ones. ', 50, 'red_rose.jpg', 'red_rose.jpg', 'flower', '2022-07-19 23:35:00', '2022-08-04 15:29:34'),
+(2, 'Lilies', 34.78, 'Directly from Netherlands, these lilies can stand the cold and decorate your homes in winter.', 45, 'lily.jpg', 'lily.jpg,lily_1.jpg', 'flower', '2022-07-19 23:38:58', '2022-07-19 23:38:58'),
+(3, 'Sunflower', 27.58, 'Beautiful Sunflowers, perfect to start a sunny day.', 41, 'sunflower.jpg', 'sunflower.jpg,sunflower_1.jpg', 'flower', '2022-07-19 23:42:24', '2022-07-19 23:42:24'),
+(4, 'The Flower of Love', 58.00, 'Looking for a gift for loved one? Look no more. This bouquet full of roses and lilies can fit any occasion.', 19, 'flower_of_love.jpg', 'flower_of_love.jpg', 'bouquet', '2022-07-19 23:45:20', '2022-07-19 23:45:20'),
+(5, 'Peony', 19.67, 'Need some fillers? These peonies does that job.', 51, 'peony.jpg', 'peony.jpg,peony_1.jpg', 'flower', '2022-07-19 23:47:36', '2022-07-19 23:47:36'),
+(6, 'Tulip', 28.75, 'Pretty unique, handmade Basket of Tulips', 38, 'tulip.jpg', 'tulip.jpg', 'bouquet', '2022-07-19 23:49:21', '2022-07-19 23:49:21'),
+(7, 'Aster', 13.93, 'This group of asters are perfect add on for any decoration. ', 84, 'aster.jpg', 'aster.jpg,aster_1.jpg', 'bouquet', '2022-07-19 23:51:13', '2022-07-19 23:51:13'),
+(8, 'Orchids', 67.14, 'Looking for a house warming gift? Get this bouquet of orchid to complement the new home.', 46, 'orchid.jpg', 'orchid.jpg', 'bouquet', '2022-07-19 23:52:25', '2022-07-19 23:52:25'),
+(9, 'Daisy', 41.72, 'Symbol of HER, perfect gift for all the females of your life.', 67, 'daisy.jpg', 'daisy.jpg', 'bouquet', '2022-07-19 23:54:15', '2022-07-19 23:54:15'),
+(10, 'Mogra', 36.91, 'Bunch of Mogras with soothing scent to calm yourself after a busy day.', 25, 'mogra.jpg', 'mogra.jpg', 'bouquet', '2022-07-19 23:56:38', '2022-07-19 23:56:38'),
+(11, 'Lotus', 31.43, 'Indian lotus to complete your new backyard pond. ', 57, 'lotus.jpg', 'lotus.jpg', 'flower', '2022-07-19 23:59:10', '2022-07-19 23:59:10'),
+(12, 'Daffodil', 54.98, 'Fresh bouquet of Daffodils to make someone\'s day.', 40, 'daffodil.jpg', 'daffodil.jpg,daffodil_1.jpg', 'bouquet', '2022-07-20 00:01:13', '2022-08-03 21:46:02');
 
 --
 -- Indexes for dumped tables
@@ -77,7 +77,7 @@ ALTER TABLE `products`
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
